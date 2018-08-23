d1a35df0b9a55bd39a1a03949b00d8e877d1f0f4
LeanKhan <eslean20@gmail.com>
2018-08-23 22:09:26 +0100

    Admin component

--- frontend/epp/src/app/app-routing/app-routing.module.ts
+++ frontend/epp/src/app/app-routing/app-routing.module.ts
@@ -4,11 +4,21 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {LandingComponent} from '../landing/landing.component';
 import {ProductPageComponent} from '../product-page/product-page.component';
+import {AdminComponent} from '../admin/admin.component';
+import {AddProductComponent} from '../admin/add-product/add-product.component';
+import {DashboardComponent} from '../admin/dashboard/dashboard.component';
+import {EditProductComponent} from '../admin/edit-product/edit-product.component';
 
 const routes : Routes = [
   {path: '', redirectTo: 'landing', pathMatch: 'full'},
   {path: 'landing', component: LandingComponent},
-  {path: 'products', component: ProductPageComponent}
+  {path: 'products', component: ProductPageComponent},
+  {path: 'admin', component: AdminComponent, children: [
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'edit-product', component: EditProductComponent},
+    {path: 'add-product', component: AddProductComponent}
+  ]}
 ]
 
 @NgModule({
--- frontend/epp/src/app/app.module.ts
+++ frontend/epp/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import {NavComponent} from './nav/nav.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AddProductComponent } from './admin/add-product/add-product.component';
+import { EditProductComponent } from './admin/edit-product/edit-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     NavComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    AdminComponent,
+    DashboardComponent,
+    AddProductComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
